3a15dc27733baeac3d886ca67545ff0375603804
SivakumarM-ks005 <sivakumar.mani@kryasolutions.com>
2025-08-06 22:12:14 +0530

    new pages

---
+++ src/app/common-components/side-nav-menu/side-nav-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-nav-menu',
+  imports: [],
+  templateUrl: './side-nav-menu.component.html',
+  styleUrl: './side-nav-menu.component.scss'
+})
+export class SideNavMenuComponent {
+
+}
