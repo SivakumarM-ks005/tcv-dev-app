00e9474304c9c3cf7e6c7b4a990e7d7b87ba7799
SivakumarM-ks005 <sivakumar.mani@kryasolutions.com>
2025-08-06 17:24:03 +0530

    updates
    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,29 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'dev-2025';
+   isSidebarOpen = true;
+  isMobile = false;
+  constructor() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isMobile = window.innerWidth <= 768; // You can adjust breakpoint
+     this.isSidebarOpen = false;
+  }
+
+  toggleSidebar() {
+    if (this.isMobile) {
+      // Mobile behavior (toggle open/close)
+      this.isSidebarOpen = !this.isSidebarOpen;
+    } else {
+      // Web behavior (toggle or expand/collapse based on need)
+      this.isSidebarOpen = !this.isSidebarOpen;
+    }
+  }
 }
